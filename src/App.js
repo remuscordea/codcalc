b7a56a152f6c30a6a65206fefb8b971139fbec0f
Remus Cordea <remus.cordea@gmail.com>
2023-11-24 17:41:26 +0200

    some UI improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import classnames from "classnames";
 import Speedups from "./Speedups";
 import Resources from "./Resources";
 import "./App.css";
@@ -7,25 +8,27 @@ import "./App.css";
 const App = () => {
   return (
     <Router>
-      <div className="pageLayout">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Speedups</Link>
-            </li>
-            <li>
-              <Link to="/resources">Resources</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className={classnames("pageLayout", "overlay")}>
+        <div className="pageContent">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Speedups</Link>
+              </li>
+              <li>
+                <Link to="/resources">Resources</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <div className={"main"}>
-          <h1>CoD Tools</h1>
+          <div className={"main"}>
+            <h1>Call of Dragons Toolbox</h1>
 
-          <Routes>
-            <Route path="/" element={<Speedups />} />
-            <Route path="/resources" element={<Resources />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Speedups />} />
+              <Route path="/resources" element={<Resources />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
